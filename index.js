4f2902fd4bf6297213cd7ce52d76ff7c4c5a624d
GalvisDeveloper <galvisdeveloper@gmail.com>
2021-09-15 15:09:56 -0500

    http server & port, donwload file csv through header

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Hello world!");
+const http = require("http");
+
+http
+  .createServer((req, res) => {
+    res.setHeader("Content-Disposition", "attachment; filename=lista.csv");
+
+    res.writeHead(200, { "Content-Type": "application/csv" });
+    // res.write("404 | Page Not Found!");
+
+    res.write("id, name \n");
+    res.write("1, Test \n");
+    res.write("2, You \n");
+    res.write("3, Done \n");
+
+    res.end();
+  })
+  .listen(8080);
+
+console.log("Listen on port", 8080);
