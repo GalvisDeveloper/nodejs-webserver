c91613c9a30f43ce98fc57b2897703dcd4f1ddf2
GalvisDeveloper <galvisdeveloper@gmail.com>
2022-09-28 04:54:09 -0500

    Handlebars y subida a hosting

--- index.js
+++ index.js
@@ -1,28 +1,59 @@
+require('dotenv').config();
 const express = require("express");
+const hbs = require("hbs");
+
 const app = express();
-const port = 8080;
+const port = process.env.PORT;
+
+// Handlebars
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
 
 // Servir contenido estático mediante middleware
 app.use(express.static("public"));
 
+app.get(["/", "/home"], (req, res) => {
+  res.status(200);
+  res.render('home', {
+    name: 'Cristian Bustos',
+    title: 'Node Course',
+  })
+});
+
 app.get("/generic", (req, res) => {
   res.status(200);
-  res.sendFile(__dirname + "/public/generic.html")
+  res.render('generic', {
+    name: 'Cristian Bustos',
+    title: 'Node Course',
+  })
 });
 
 app.get("/elements", (req, res) => {
   res.status(200);
-  res.sendFile(__dirname + "/public/elements.html")
+  res.render('elements', {
+    name: 'Cristian Bustos',
+    title: 'Node Course',
+  })
 });
 
-app.get("/home", (req, res) => {
-  res.status(200);
-  res.sendFile(__dirname + "/public/index.html")
-});
+// app.get("/generic", (req, res) => {
+//   res.status(200);
+//   res.sendFile(__dirname + "/public/back/template/generic.html")
+// });
+
+// app.get("/elements", (req, res) => {
+//   res.status(200);
+//   res.sendFile(__dirname + "/public/back/template/elements.html")
+// });
+
+// app.get("/home", (req, res) => {
+//   res.status(200);
+//   res.sendFile(__dirname + "/public/back/template/index.html")
+// });
 
 app.get("*", (req, res) => {
   res.status(404);
-  res.sendFile(__dirname + "/public/404.html");
+  res.sendFile(__dirname + "/public/back/404.html");
 });
 
 app.listen(port, () => {
